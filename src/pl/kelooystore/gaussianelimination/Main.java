4339985587f39b3de8acabc1b78c508b018f82de
kelooy <karol.osowski.97@gmail.com>
2020-04-05 18:36:10 +0200

    Initial commit

---
+++ src/pl/kelooystore/gaussianelimination/Main.java
@@ -0,0 +1,47 @@
+package pl.kelooystore.gaussianelimination;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+import static pl.kelooystore.gaussianelimination.GaussianElimination.solveMatrix;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of variables: ");
+        int n = scanner.nextInt();
+        System.out.println();
+
+        double[][] matrixA = new double[n][n];
+        double[] matrixB = new double[n];
+
+        System.out.println("Left side of matrix.\nEnter " + n + " coefficients if variables:\n");
+        for (int i = 0; i < n; i++) {
+            System.out.println("The "+(i+1)+". row is...");
+            for (int j = 0; j < n; j++) {
+                matrixA[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.println("\nRight side of matrix.\nEnter " + n + " solutions:");
+        for (int i = 0; i < n; i++) {
+            matrixB[i] = scanner.nextDouble();
+        }
+
+        double[] matrixC = solveMatrix(matrixA, matrixB);
+
+        System.out.println("\nResulting matrix:\n");
+        for (int i = 0; i < n; i++) {
+            System.out.println(matrixC[i]);
+        }
+
+        System.out.println(matrixA);
+
+    }
+
+}
+
+
